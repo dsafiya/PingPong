fc03fe45b1d62384e4509d2bf01569ff98f98f40
Safiya <110724983+dsafiya@users.noreply.github.com>
2022-08-07 11:24:21 +0300

    Update pingpong.py
    Добавлены классы GAmeSprite и Player созданы и отрисованы экземпляры этих классов

--- pingpong.py
+++ pingpong.py
@@ -1,10 +1,48 @@
 from turtle import window_height
 from pygame import*
 
+# класс-родитель для других спрайтов
+class GameSprite(sprite.Sprite):
+  # конструктор класса
+    def __init__(self, player_image, player_x, player_y, player_speed, wight, height):
+        # Вызываем конструктор класса (Sprite):
+        super().__init__()
+
+        # каждый спрайт должен хранить свойство image - изображение
+        self.image = transform.scale(image.load(player_image), (wight, height))
+        self.speed = player_speed
+
+        # каждый спрайт должен хранить свойство rect - прямоугольник, в который он вписан
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+ 
+  # метод, отрисовывающий героя на окне
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+# класс главного игрока
+class Player(GameSprite):
+    # метод для управления спрайтом стрелками клавиатуры
+    def update_r(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed
+  # метод "выстрел" (используем место игрока, чтобы создать там пулю)
+    def update_l(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed     
+        # класс главного игрока
+
 back = (200, 255, 255)
 win_width = 600
-window_height = 500
-window = display.set_mode((win_width, window_height))
+win_height = 500
+window = display.set_mode((win_width, win_height))
 window.fill(back)
 
 game = True
@@ -12,9 +50,19 @@ finish = False
 clock = time.Clock()
 FPS = 60
 
+racket1 = Player('racket.png', 30, 200, 4, 50, 150)
+racket2 = Player('racket.png', 520, 200, 4, 50, 150)
+ball = GameSprite('tennis.png', 200, 200, 4, 50, 50)
+
 while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
+
+    racket1.reset()
+    racket2.reset()
+    ball.reset()        
+
+
     display.update()
     clock.tick(FPS)   
