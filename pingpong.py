f7f6067cfabe020a463b6771264cbdc225271370
Safiya <110724983+dsafiya@users.noreply.github.com>
2022-08-13 16:24:56 +0300

    Add files via upload

--- pingpong.py
+++ pingpong.py
@@ -54,10 +54,42 @@ racket1 = Player('racket.png', 30, 200, 4, 50, 150)
 racket2 = Player('racket.png', 520, 200, 4, 50, 150)
 ball = GameSprite('tennis.png', 200, 200, 4, 50, 50)
 
+font.init()
+font = font.SysFont('Arial', 40)
+lose1= font.render('PLAYER 1 LOSE', True, (180, 0, 0))
+lose2 = font.render('PLAYER 2 LOSE', True, (180, 0, 0))
+
+speed_x = 3
+speed_y = 3
+
 while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
+
+    if finish != True:
+        window.fill(back)
+        racket1.update_l()
+        racket2.update_r()
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+
+        if sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket2, ball):
+            speed_x *= -1
+
+        if ball.rect.y > win_height-50 or ball.rect.y <0:
+            speed_y *= -1 
+
+        if ball.rect.x < -60:
+            finish = True
+            window.blit(lose1, (200, 200))
+            game_over = True
+
+        if ball.rect.x > win_width:
+            finish = True 
+            window.blit(lose2, (200, 200))
+            game_over = True  
+
 
     racket1.reset()
     racket2.reset()
